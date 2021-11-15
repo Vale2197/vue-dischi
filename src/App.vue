157308f01cf802267608006301fea08dfe2e5875
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-15 12:09:33 +0100

    create variabili colori, importato bootstrap, creato componente
    siteHeader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <siteHeader/>
+   <!--  <siteMain/> -->
+    <!-- <siteFooter/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import siteHeader from "./components/siteHeader.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    siteHeader
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/bootstrap";
+@import "./assets/variabili.scss";
+
 </style>
---
+++ src/components/siteHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: "siteHeader",
+}
+</script>
+
+<style lang="scss">
+    
+</style>
